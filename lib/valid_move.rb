352ef752ada9a444f204d50031b87ca425a1d696
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-06 15:03:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,8 @@
 def valid_move?(board, position_taken)
 
-  if position_taken.between?(0,8) != true
+  if position_taken.between?(0,8) != true && board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
     false
   else
     true
   end
 end
-
-    def position_taken?(board, index_number)
-      if board[index_number] == " " || board[index_number] == "" || board[index_number] == nil
-        false
-      else
-        true
-      end
-    end
